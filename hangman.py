d9365d2b4e23e9b2944cd0a86125394b206fcdb5
Daniel Pitulia <daniel@dpitulia.com>
2022-07-08 22:52:19 +0200

    Fixed errors in functions and added hangman string

--- hangman.py
+++ hangman.py
@@ -8,20 +8,27 @@
 import random
 import re
 
-alternatives = ["javascript","python", "bash", "perl", "github", "raspberrypi"]
+alternatives = ["javascript"]
 count_fail = 0
-hangman = """\
-###########################
-    #              ##
-    O              ##
-   \|/             ##
-    |
-   / \             ##
-                   ##
-                   ##
-                   ##
-"""
 
+hangman = [
+    "\t O\t\t##",
+    "\t\|/\t\t##",
+    "\t | \t\t##",
+    "\t/ \ \t\t##",
+]
+
+hang_pole = [
+    "###########################",
+    "\t #\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+]
 # Randomly choose a word from the list of alternatives
 answer = alternatives[random.randrange(0,len(alternatives))]	# String of answer
 answer_remaining = list(answer)		# List of answer
@@ -32,48 +39,57 @@ answer_remaining = list(answer)		# List of answer
 progress = "_"*len(answer)
 progress = list(progress)
 
-indx_matches = []
 
-print("Welcome to Hangman for programmers!\n Guess the following word using lowercase: ",
-"".join(progress), "\n Guess a character :)")
+print("Welcome to Hangman for programmers!")
+for line in hang_pole:
+    print(line)
+
+print("Find the following word by guessing matching characters: ")
+print("".join(progress), "\nHint: the word is a programming language. You are allowed four incorrect guesses.")
 
 # While progress is not completed, prompt a guess, use re.finditer to find matches
 # and store these in indx_matches.
 
-def find_matches_and_save(guess, answer):
-	global indx_matches
+def find_matches(guess, answer):
+    """ Search guess in answer using re.finditer, find indexes, remove succesful
+    guess from answer_remaining[], add succesful guess to progress[] and return
+    answer_remaining[] and progress[]"""
 
-	""" Search guess in answer using re.finditer, find indexes, remove succesful
-	guess from answer_remaining[], add succesful guess to progress[] and return
-	answer_remaining[] and progress[]"""
-
-	matches = re.finditer(guess, answer)
+    indx_matches = []
+    matches = re.finditer(guess, answer)
 
 
-	for m in matches:
-		indx_matches.append(m.span()[0]) # Store index of match from m.span()[0]
+    for m in matches:
+        indx_matches.append(m.span()[0]) # Store index of match from m.span()[0]
 
-	# Save result
+        # Save result
+    return indx_matches
 
-	for indx in indx_matches:
-		answer_remaining[indx] = ""	# Removes correct guess from answer_remaining
-		progress[indx] = guess	# Adds correct guess to the progress list
 
-	return answer_remaining, progress
 
-def print_result(answer_is):
-	global count_fail	# Not sure about the correct way of including count_fail. I used global.
-	if answer_is == "correct":
-		print(hangman + "\n Correct guess: " + guess + "".join(progress))
-	else:
-		count_fail +=1
-		print(hangman + "\n Failed guess: " + guess + "\n" + "Times failed: " + str(count_fail))
-	return count_fail
+
+
 
 while "_" in progress:
-	guess = input()
-	if guess in answer:
-		find_matches_and_save(guess, answer)
-		print_result("correct")
-	else:
-		print_result("fail")
+    guess = input()
+    if guess in answer:
+        indx = find_matches(guess, answer)
+        for i in indx:
+            answer_remaining[i] = ""
+            progress[i] = guess
+
+
+        for line in hang_pole:
+            print(line)
+        print("Correct guess: " + "".join(progress))
+    else:
+        count_fail +=1
+        hang_pole[count_fail+1] = hangman[count_fail-1]
+
+        for line in hang_pole:
+            print(line)
+        print("Failed guess: " + guess + "\n" + "Times failed: " + str(count_fail))
+
+    if(count_fail > 3):
+        print("Sorry, you're dead :(")
+        break
--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import re
-hangman = """\
-###########################
-    #              ##
-    O              ##
-   \|/             ##
-    |
-   / \             ##
-                   ##
-                   ##
-                   ##
-"""
-print(hangman)
+hangman = [
+    "###########################",
+    "\t #\t\t##",
+    "\t O\t\t##",
+    "\t\|/\t\t##",
+    "\t | \t\t##",
+    "\t/ \ \t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+    "\t\t\t##",
+]
+for line in hangman:
+    print(line)
